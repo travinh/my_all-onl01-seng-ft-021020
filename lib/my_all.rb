dcd815bb5e9485c76442d5e8571ed64652b3c4a7
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-14 07:52:52 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,5 +7,5 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i = i+1
   end
-  block_return_values.include? (false) ? false : true
+  block_return_values.include?(false) ? false : true
 end
